587bb2b4766715d3f75b3cba7627462db8cbc817
Hendi Santika <hendisantika@gmail.com>
2017-09-11 21:12:44 +0700

    #2 - Added Main App - Restful Service

---
+++ RestfulService/src/main/java/com/hendisantika/restservice/RestServicesApplication.java
@@ -0,0 +1,21 @@
+package com.hendisantika.restservice;
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/10/17
+ * Time: 6:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesApplication.class, args);
+	}
+}
